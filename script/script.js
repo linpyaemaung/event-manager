f6ffaca786cb2d4f61bde8e78022b2d0f6e692c1
Lin Pyae Maung <linpyaemg.lpmaung@gmail.com>
2025-11-17 22:38:56 +0630

    second commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+    const images = document.querySelectorAll('img');
+    let index = 0;
+
+    setInterval(() => {
+      images[index].classList.remove('active');
+      index = (index + 1) % images.length;
+      images[index].classList.add('active');
+    }, 3000);
